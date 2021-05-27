c1f297eef3140df81f843a500a80cd6d1b2be0f9
a876691666 <928200728@qq.com>
2021-05-27 22:50:21 +0800

    add jump

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import * as THREE from "three";
 import "./App.css";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import data from "./systemPosition.json";
+import jump from "./systemJump.json";
 import vertex from "./vertex.glsl";
 import fragment from "./fragment.glsl";
 
@@ -32,29 +33,57 @@ class App extends React.Component {
     renderer.setSize(window.innerWidth, window.innerHeight);
     const control = new OrbitControls(camera, renderer.domElement);
 
-    const ids = Object.keys(data);
-    const position = [];
-    ids.forEach((id) => {
-      const [x, y, z] = data[id];
-      position.push(x * scale, y * scale, z * scale);
-    });
+    {
+      const ids = Object.keys(data);
+      const position = [];
+      ids.forEach((id) => {
+        const [x, y, z] = data[id];
+        position.push(x * scale, y * scale, z * scale);
+      });
 
-    const geometry = new THREE.BufferGeometry();
-    geometry.setAttribute(
-      "position",
-      new THREE.Float32BufferAttribute(position, 3)
-    );
+      const geometry = new THREE.BufferGeometry();
+      geometry.setAttribute(
+        "position",
+        new THREE.Float32BufferAttribute(position, 3)
+      );
 
-    const material = new THREE.ShaderMaterial({
-      uniforms: {
-        color: { value: new THREE.Color(0xffffff) },
-      },
-      vertexShader: vertex,
-      fragmentShader: fragment,
-    });
-    const point = new THREE.Points(geometry, material);
+      const material = new THREE.ShaderMaterial({
+        uniforms: {
+          color: { value: new THREE.Color(0xffffff) },
+        },
+        vertexShader: vertex,
+        fragmentShader: fragment,
+      });
+      const point = new THREE.Points(geometry, material);
 
-    scene.add(point);
+      scene.add(point);
+    }
+    {
+      const material = new THREE.LineBasicMaterial({
+        color: 0x0000ff,
+      });
+
+      const points = [];
+      console.log(jump);
+      jump.forEach(([source, target]) => {
+        {
+          const [x, y, z] = data[target];
+          points.push(new THREE.Vector3(x * scale, y * scale, z * scale));
+        }
+        {
+          const [x, y, z] = data[source];
+          points.push(new THREE.Vector3(x * scale, y * scale, z * scale));
+        }
+      });
+      points.push(new THREE.Vector3(-10, 0, 0));
+      points.push(new THREE.Vector3(0, 10, 0));
+      points.push(new THREE.Vector3(10, 0, 0));
+
+      const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+      const line = new THREE.LineSegments(geometry, material);
+      scene.add(line);
+    }
 
     camera.position.z = 5;
 
--- src/vertex.glsl
+++ src/vertex.glsl
@@ -1,6 +1,6 @@
 attribute float scale;
 void main() {
     vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
-    gl_PointSize = 50.0;
+    gl_PointSize = 2.0;
     gl_Position = projectionMatrix * mvPosition;
 }
