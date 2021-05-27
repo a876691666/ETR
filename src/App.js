9e3a8fc3227ca833179817c5ce698a338048dfa0
undefined <undefined>
2021-05-27 18:37:27 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as THREE from "three";
+import "./App.css";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import data from "./systemPosition.json";
+import vertex from "./vertex.glsl";
+import fragment from "./fragment.glsl";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const scale = 0.0001;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.getRef = this.getRef.bind(this);
+  }
+
+  render() {
+    return <div className="App" ref={(box) => (this.box = box)}></div>;
+  }
+
+  componentDidMount() {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      10000
+    );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    const control = new OrbitControls(camera, renderer.domElement);
+
+    const ids = Object.keys(data);
+    const position = [];
+    ids.forEach((id) => {
+      const [x, y, z] = data[id];
+      position.push(x * scale, y * scale, z * scale);
+    });
+
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute(
+      "position",
+      new THREE.Float32BufferAttribute(position, 3)
+    );
+
+    const material = new THREE.ShaderMaterial({
+      uniforms: {
+        color: { value: new THREE.Color(0xffffff) },
+      },
+      vertexShader: vertex,
+      fragmentShader: fragment,
+    });
+    const point = new THREE.Points(geometry, material);
+
+    scene.add(point);
+
+    camera.position.z = 5;
+
+    this.box.appendChild(renderer.domElement);
+
+    function animate() {
+      requestAnimationFrame(animate);
+      control.update();
+      renderer.render(scene, camera);
+    }
+    animate();
+  }
+
+  getRef(box) {
+    this.box = box;
+  }
 }
 
 export default App;
